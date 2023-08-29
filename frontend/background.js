046913f1afd73909f57fff17700339cd35be378e
SpectralGT <atharvsingh1396@gmail.com>
2023-08-29 17:32:14 +0530

    changed object position

--- frontend/background.js
+++ frontend/background.js
@@ -5,12 +5,12 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 20;
+camera.position.z = 10;
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
@@ -26,14 +26,26 @@ document.body.append(renderer.domElement);
 let objects = [];
 const loader = new GLTFLoader();
 
-for (let i = 0; i < 10; i++) {
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshNormalMaterial();
-  let cube = new THREE.Mesh(geometry, material);
+for (let i = 0; i < 3; i++) {
   loader.load(
     "./capsule.gltf",
     function (gltf) {
-      cube = gltf.scene.children[0];
+      let cube = gltf.scene.children[0];
+      scene.add(cube);
+      objects.push(cube);
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    }
+  );
+}
+
+for (let i = 0; i < 2; i++) {
+  loader.load(
+    "./curl.gltf",
+    function (gltf) {
+      let cube = gltf.scene.children[0];
       scene.add(cube);
       objects.push(cube);
     },
@@ -59,9 +71,10 @@ document.onmousemove = (e) => {
 function animate() {
   requestAnimationFrame(animate);
 
-  objects.map((c, i) => {
-    c.rotation.x += (ObjectData[i].offset.x + 1) * 0.001;
-    c.rotation.y += (ObjectData[i].offset.y + 1) * 0.001;
+  objects.map((o, i) => {
+    o.rotation.y = ObjectData[i].rotation.y * (Math.PI / 180);
+    o.rotation.z = ObjectData[i].rotation.z * (Math.PI / 180);
+    console.log(o.rotation);
   });
 
   renderer.render(scene, camera);

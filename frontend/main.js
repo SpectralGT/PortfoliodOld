4592d21224e347beeb8d1557359de1e85a149fda
SpectralGT <atharvsingh1396@gmail.com>
2023-09-13 15:11:36 +0530

    created a circle to follow the cursor

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("mousemove", function (e) {
+  let body = document.querySelector("body");
+  let circle = document.getElementById("circle");
+  let left = e.pageX;
+  let top = e.pageY;
+  circle.style.left = left + "px";
+  circle.style.top = top + "px";
+});

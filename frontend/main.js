a32bc5b384f35cb44e6e239b1909106ce8632ad4
SpectralGT <atharvsingh1396@gmail.com>
2023-10-03 20:28:41 +0530

    updated contact page

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,4 @@
-document.addEventListener("mousemove", function (e) {
-  let body = document.querySelector("body");
-  let circle = document.getElementById("circle");
-  let left = e.pageX;
-  let top = e.pageY;
-  circle.style.left = left + "px";
-  circle.style.top = top + "px";
+window.addEventListener("resize", function () {
+  "use strict";
+  window.location.reload();
 });
